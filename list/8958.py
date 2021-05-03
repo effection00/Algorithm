f322417c68c681ec102da84d1419169df03667a0
effection00 <effection97@gmail.com>
2021-05-04 01:13:31 +0900

    list

--- list/8958.py
+++ list/8958.py
@@ -1,7 +1,9 @@
+# 8958
 n= int(input())
 for i in range(n): #n번의 ox값
   score = 0
   sum = 0
+  # 8958
   a = input()
   for e in a: #문자열 한개씩 나옴
     if e == 'O': #o인 경우
@@ -16,7 +18,8 @@ for i in range(n): #n번의 ox값
 #1 3 3 3 4 6 
 #o o o x o o o
 #1 2 3 0 1 2 3 = 12 
-#1 3 6 6 7 9 12
+#1 3 6 6 7 9 12 
+
 
    
     
